deb5c89c50a66a35d281f2d80035e5f2bf0bea29
Jim Hawker <j.d.hawker@googlemail.com>
2023-05-31 16:19:31 +0100

    width 75 percent

--- components/content/Gallery.vue
+++ components/content/Gallery.vue
@@ -39,7 +39,7 @@ css({
       },
       img: {
         objectFit: 'cover',
-        width: '30%',
+        width: '75%',
         aspectRatio: '9 / 16',
         borderRadius: '{radii.md}'
       }
